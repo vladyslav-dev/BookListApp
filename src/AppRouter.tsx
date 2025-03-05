82a4fd1033c2811e88e6faa356dd9bb0728fd4cf
vladyslav-dev <grigovlad09112002@gmail.com>
2025-03-06 00:35:32 +0100

    Setup react router

---
+++ src/AppRouter.tsx
@@ -0,0 +1,30 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+} from "react-router-dom";
+
+/* Pages */
+import Dashboard from '@/pages/Dashboard'
+import BookForm from '@/pages/BookForm'
+
+const router = createBrowserRouter([
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+  },
+  {
+    path: "/book-form",
+    element: <BookForm />,
+  },
+  {
+    path: "*",
+    element: <Navigate to="/dashboard" />,
+  },
+])
+
+const AppRouter = () => (
+  <RouterProvider router={router} />
+)
+
+export default AppRouter
---
+++ src/pages/BookForm.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BookFormPage = () => {
+  return (
+    <div>BookFormPage</div>
+  )
+}
+
+export default BookFormPage
